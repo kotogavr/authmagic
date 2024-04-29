38e34e83f47aa52ce7bacad432f36a6284528f24
Ann <59127075+kotogavr@users.noreply.github.com>
2024-04-29 17:38:14 +0800

    fix user model proverka

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ApplicationRecord
   has_many :sessions, dependent: :destroy
   has_many :sign_in_tokens, dependent: :destroy
 
-  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  normalizes :email, with: -> { _1.strip.downcase }
 end
