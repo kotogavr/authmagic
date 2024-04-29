182fd7857723e6924758c86e37ef54fe25339887
Ann <59127075+kotogavr@users.noreply.github.com>
2024-04-29 14:49:30 +0800

    add user associations and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_many :sessions, dependent: :destroy
+  has_many :sign_in_tokens, dependent: :destroy
+
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
