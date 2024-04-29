fcbd0aa12c0f974c7c891fc8fadc6f104c49d61e
Ann <59127075+kotogavr@users.noreply.github.com>
2024-04-29 14:45:55 +0800

    generate auth models

---
+++ db/migrate/20240429064513_create_sign_in_tokens.rb
@@ -0,0 +1,9 @@
+class CreateSignInTokens < ActiveRecord::Migration[7.1]
+  def change
+    create_table :sign_in_tokens do |t|
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
