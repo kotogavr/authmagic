b9b731eb5fa2a25013a11a8327c5df4022b22048
Ann <59127075+kotogavr@users.noreply.github.com>
2024-04-30 19:27:20 +0800

    create user mailer

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class UserMailer < ApplicationMailer
+  def passwordless
+    @user = params[:user]
+    @signed_id = @user.sign_in_token.create.signed_id(expires_in: 1.day)
+
+    mail to: @user.email, subject: 'Your sign in link'
+  end
+end
