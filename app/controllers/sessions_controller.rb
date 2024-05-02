3e6b1e4c208877814fe45466eea5724f46d8901b
Ann <59127075+kotogavr@users.noreply.github.com>
2024-05-02 14:24:35 +0800

    upd: verify and mailer  in session_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,22 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    User.find_or_create_by(email: params[:email])
-    # TODO: add mailer to user
+    user = User.find_or_create_by(email: params[:email])
+    UserMailer.with(user:).passwordless.deliver_later
     redirect_to login_path, notice: 'Check your email for a link to sign in'
   end
 
-  def verify; end
+  def verify
+    begin
+      user = SignInToken.find_signed!(params[:sid])
+    rescue StandardError
+      redirect_to login_path, alert: 'Invalid or expired token'
+    end
+    session_record = user.sessions.create!
+    cookies.signed.permanent[:session_token] = { value: session_record.id, httponly: true }
+    user.sign_in_tokens.delete_all
+    redirect_to root_path, notice: 'Successfully signed in'
+  end
 
   def destroy; end
 end
