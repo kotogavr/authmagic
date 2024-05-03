0421a8db4c554228cbfa2caf8d7d14d668743a41
Ann <59127075+kotogavr@users.noreply.github.com>
2024-05-03 14:48:59 +0800

    fix naming

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@
 class UserMailer < ApplicationMailer
   def passwordless
     @user = params[:user]
-    @signed_id = @user.sign_in_token.create.signed_id(expires_in: 1.day)
+    @signed_id = @user.sign_in_tokens.create.signed_id(expires_in: 1.day)
 
     mail to: @user.email, subject: 'Your sign in link'
   end
