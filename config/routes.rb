049bef6d7c2b49e20ee60d840faa1095f9e8e7b4
Ann <59127075+kotogavr@users.noreply.github.com>
2024-05-04 15:47:45 +0800

    add Home as root path

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,12 @@ Rails.application.routes.draw do
   get 'sessions/create'
   get 'sessions/verify'
   get 'sessions/destroy'
+
   get 'login' => 'sessions#new', as: :login
   get 'up' => 'rails/health#show', as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # resources :sessions, only: %i[create destroy] do
+  #  get :verify, on: :collection
+  # end
+  root 'home#index'
 end
