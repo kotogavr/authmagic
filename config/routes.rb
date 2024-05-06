7f2bc64853a1d0625de2494bc9bd0c7397d4f18f
Ann <59127075+kotogavr@users.noreply.github.com>
2024-05-06 15:56:13 +0800

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/verify'
-  get 'sessions/destroy'
-
   get 'login' => 'sessions#new', as: :login
   get 'up' => 'rails/health#show', as: :rails_health_check
 
-  # resources :sessions, only: %i[create destroy] do
-  #  get :verify, on: :collection
-  # end
+  resources :sessions, only: %i[create destroy] do
+    get :verify, on: :collection
+  end
   root 'home#index'
 end
